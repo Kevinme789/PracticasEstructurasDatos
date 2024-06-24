a7a7dfc5e43f3059eea96dae8094de8f58faf433
kevin <porkevin743@gmail.com>
2024-06-24 16:56:18 -0500

    Ejercicio06 SegundoB

---
+++ Ejercicio06/src/main/java/listas/ejercicio06/Ejercicio06.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package listas.ejercicio06;
+
+/**
+ *
+ * @author KevinR
+ */
+public class Ejercicio06 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
