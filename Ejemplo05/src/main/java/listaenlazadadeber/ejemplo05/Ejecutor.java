178d3de7a0db925a20e95390ec06da901503b5bc
kevin <porkevin743@gmail.com>
2024-06-24 16:55:49 -0500

    Ejercicio05 SegundoB

---
+++ Ejemplo05/src/main/java/listaenlazadadeber/ejemplo05/Ejecutor.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package listaenlazadadeber.ejemplo05;
+
+/**
+ *
+ * @author KevinR
+ */
+public class Ejecutor {
+
+    public static void main(String[] args) {
+        Metodos obj = new Metodos();
+        int opc;
+        do{
+        opc = obj.menu();
+            switch(opc){
+                case 1:
+                    obj.llenar();
+                    break;
+                case 2:
+                    System.out.println("Ingrese el numero");
+                    int num = obj.entrada.nextInt();
+                    obj.eliminar(num);
+                    break;
+                case 3:
+                    obj.recorrer();
+                    break;
+            }
+        }while(opc != 0);
+    }
+}
