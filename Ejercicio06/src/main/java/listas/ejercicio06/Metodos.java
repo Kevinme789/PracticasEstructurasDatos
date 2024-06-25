1e3b3d2446b83a9e278c173a13c0bd73df1f091a
kevin <porkevin743@gmail.com>
2024-06-25 13:07:26 -0500

    Ejercicio06 SegundoB

---
+++ Ejercicio06/src/main/java/listas/ejercicio06/Metodos.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package listas.ejercicio06;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author KevinR
+ */
+public class Metodos {
+
+    Scanner entrada;
+    Nodo head;
+    Nodo tail;
+
+    public Metodos() {
+        this.entrada = new Scanner(System.in);
+        this.head = null;
+        this.tail = null;
+    }
+
+    public int menu() {
+        System.out.println("[1] Ingresar un nuevo Nodo");
+        System.out.println("[2] Ingresar ordenacion y presentar en pantalla");
+        return entrada.nextInt();
+    }
+
+    public void insertar(char letra) {
+        Nodo nodo = new Nodo(letra);
+        if (head == null) {
+            head = nodo;
+            tail = nodo;
+        } else {
+            tail.sig = nodo;
+            tail = nodo;
+
+        }
+    }
+
+   public Nodo invertir() {
+        Nodo invertida = null;
+        Nodo nuevo2 = head;
+
+        while (nuevo2 != null) {
+            Nodo nuevoNodo = new Nodo(nuevo2.letra);
+            nuevoNodo.sig = invertida; 
+            invertida = nuevoNodo; 
+            nuevo2 = nuevo2.sig;
+        }
+
+        return invertida; // Devolver la cabeza de la lista invertida
+    }
+
+    public void recorrer() {
+        Nodo nuevo = head;
+        while (nuevo != null) {
+            System.out.printf(nuevo.letra + "   ");
+            nuevo = nuevo.sig;
+        }
+        System.out.println("");
+    }
+
+    public void recorrer(Nodo _head) {
+        Nodo nuevo = _head;
+        while (nuevo != null) {
+            System.out.printf(nuevo.letra + "   ");
+            nuevo = nuevo.sig;
+        }
+        System.out.println("");
+    }
+}
---
+++ Ejercicio06/src/main/java/listas/ejercicio06/Nodo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package listas.ejercicio06;
+
+/**
+ *
+ * @author KevinR
+ */
+public class Nodo {
+
+    char letra;
+    Nodo sig;
+
+    public Nodo(char _letra) {
+        this.letra = _letra;
+        this.sig = null;
+    }
+}
